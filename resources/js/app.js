5793f34d1c073ae1a914434fe1718e1c4fb62977
Medjadder Aimen <medjadder.aimen@gmail.com>
2022-02-08 15:32:56 +0100

    fix ui & breeze conflict

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,12 @@
+require('./bootstrap');
+
+require('alpinejs');
+
 /**
  * First we will load all of this project's JavaScript dependencies which
  * includes Vue and other libraries. It is a great starting point when
  * building robust, powerful web applications using Vue and Laravel.
  */
-
-require('./bootstrap');
 
 window.Vue = require('vue').default;
 
